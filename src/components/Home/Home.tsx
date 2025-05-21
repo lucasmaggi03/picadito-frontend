ccdb4b176d0b61efac27a6bf0d792934b144d771
Agus Gil <agussv360@gmail.com>
2025-05-21 09:03:35 -0300

    Front-End & Back-End Connection
    - Home now has the ability to get calls from the back-end

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,67 @@
 import './Home.css';
 import { IoIosArrowDown } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 import  gsap from "gsap";
 import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect, useState } from 'react';
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 export function Home() {
+    const [locations, setLocations] = useState<{ idlocation: number; location: string}[]>([]);
+    const [search, setSearch] = useState<string>("");
+    const [filteredLocations, setFilteredLocations] = useState<string[]>([]);
+    const [showSuggestions, setShowSuggestions] = useState<boolean>(false);
+    const [selectedLocation, setSelectedLocation] = useState<number | null>(null);
+    const navigate = useNavigate();
+    const [sport, setSport] = useState<string>("");
+    const [date, setDate] = useState<string>("");
+
+    useEffect(() => {
+        const getLocation = async () => {
+            try {
+                const response = await axios.get("http://localhost:5000/locations");
+                setLocations(response.data);
+            } catch (error) {
+                console.error("Error obteniendo las localidades:", error);
+            }
+        };
+        getLocation()
+    }, []);
+
+    useEffect(() => {
+        if (search.length > 0) {
+            const results = locations
+                .filter((loc) => loc.location.toLowerCase().includes(search.toLowerCase()))
+                .map((loc) => loc.location);
+            setFilteredLocations(results);
+            setShowSuggestions(true);
+        } else {
+            setFilteredLocations([]);
+            setShowSuggestions(false);
+        }
+    }, [search, locations]);
+
+    const handleSelectLocation = (locName: string) => {
+        setSearch(locName);
+        setShowSuggestions(false);
+        const locationFound = locations.find((loc) => loc.location === locName);
+        if (locationFound) {
+            setSelectedLocation(locationFound.idlocation);
+        }
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (!selectedLocation) {
+            alert("Por favor, selecciona una ubicación.");
+            return;
+        }
+        navigate("/complexes", { state: { selectedLocation, sport, date } });
+    };
     
 
     useGSAP( () => {
@@ -59,12 +113,41 @@ export function Home() {
             <div className='reservar'>
                 <div className='reservar-box'>
                     <h1>RESERVA TU PARTIDO</h1>
-                    <div className='reservar-text'>
-                        <input type="text" placeholder='Buscar Ciudad' />
-                        <input type="text" placeholder='Elige Cancha' />
-                        <input type="date" placeholder='Hora' />
+
+                    <form className='search-form' onSubmit={handleSubmit}>
+                        <div className='search'>
+                        <div className='search-text'>
+                            <input type='text' value={search} onChange={(e) => setSearch(e.target.value)} onFocus={() => setShowSuggestions(true)} onBlur={() => setTimeout(() => setShowSuggestions(false), 200)} placeholder='Buscar Ciudad' />
+                        {showSuggestions && filteredLocations.length > 0 && (
+                            <ul className='suggestions'>
+                                {filteredLocations.map((loc,index) => (
+                                    <li key={index} onClick={() => handleSelectLocation(loc)}> {loc} </li>
+                                ))}
+                            </ul>
+                        )}
+                        </div>
+                        
+                        <div className='search-text'>
+                            <select value={sport} onChange={(e) => setSport(e.target.value)}>
+                            <option value="">Elige cancha</option>
+                              <option value="f5">Fútbol 5</option>
+                              <option value="f6">Fútbol 6</option>
+                              <option value="f7">Fútbol 7</option>
+                              <option value="f8">Fútbol 8</option>
+                              <option value="f9">Fútbol 9</option>
+                              <option value="f10">Fútbol 10</option>
+                              <option value="f11">Fútbol 11</option>
+                            </select>
+                        </div>
+
+                        <div className='search-text'>
+                            <input type="date" value={date} onChange={(e) => setDate(e.target.value)} min={new Date().toISOString().split("T")[0]} />
+                        </div>
                     </div>
-                    <button className='reservar-button'>Buscar Canchas</button>
+
+                    <button className='search-button' type="submit">Buscar canchas</button> 
+                    </form>
+                    
                 </div>
             </div>
         </>
